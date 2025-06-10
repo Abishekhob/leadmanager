4109c18ad90565e6e95cb27d6ecae4d17e0d7c99
Abishekh <abishekhjuve@gmail.com>
2025-06-10 15:26:27 +0530

    Updated axios instance 4

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,19 +2,12 @@
   import axios from 'axios';
 
   const instance = axios.create({
-<<<<<<< HEAD
-<<<<<<< HEAD
-    baseURL: "https://leadmanager-wfaq.onrender.com", // base URL for your Spring Boot backend
-=======
-     baseURL: process.env.REACT_APP_API_BASE_URL,  // base URL for your Spring Boot backend
->>>>>>> 12c575d (Updated axios instance 2)
-=======
-     baseURL: process.env.REACT_APP_API_BASE_URL, // base URL for your Spring Boot backend
->>>>>>> 327dcda (Updated axios instance 3)
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 
   // Add Authorization token from localStorage to every request
   instance.interceptors.request.use(
