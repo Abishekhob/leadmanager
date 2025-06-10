18527276961338eaf488f1830744f66d4e182cd2
Abishekhob <135692354+Abishekhob@users.noreply.github.com>
2025-06-10 15:45:39 +0530

    Update axiosInstance.js

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@
   import axios from 'axios';
 
   const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://leadmanager-wfaq.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
