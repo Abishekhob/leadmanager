ec05d013a53da1cb51a601c3ffbbca9c4ba0ecea
Abishekhob <135692354+Abishekhob@users.noreply.github.com>
2025-06-10 21:47:51 +0530

    Update SecurityConfig.java 3

--- backend/src/main/java/com/example/leadmanager/config/SecurityConfig.java
+++ backend/src/main/java/com/example/leadmanager/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://leadmanager-gqd4xcoti-abishekhobs-projects.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://*.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
