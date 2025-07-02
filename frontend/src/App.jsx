59a33afd12fe962e4ef9b623adf54b2eb17f8ba3
Abishekh <abishekhjuve@gmail.com>
2025-07-02 13:24:04 +0530

    Add admin registration endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import HomePage from './pages/HomePage';
+import AdminRegisterForm from './components/AdminRegisterForm';
 
 
 function App() {
